d0e3b2db422f76205986b38d49dfe58299833119
MxAziz <hereabdulaziz@gmail.com>
2024-12-23 02:44:46 +0600

    complete Special offers page

---
+++ src/Pages/HomePage/SpecialOffers.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const SpecialOffers = () => {
+  const offers = [
+    {
+      title: "Get 15% off for weekend rentals!",
+      description:
+        "Book your car for the weekend and enjoy a 15% discount on your rental.",
+      buttonText: "Learn More",
+    },
+    {
+      title: "Luxury cars at $99/day this holiday season!",
+      description:
+        "Celebrate the holidays with premium luxury cars at an unbeatable price.",
+      buttonText: "Book Now",
+    },
+  ];
+
+  return (
+    <section className="py-16 bg-gray-50">
+      <div className="container mx-auto px-6">
+        <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
+          Special Offers
+        </h2>
+        {/* 2 offer card */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          {offers.map((offer, index) => (
+            <div
+              key={index}
+              className="p-6 bg-white rounded-lg shadow-lg transform   hover:scale-105 transition-transform duration-300"
+            >
+              <h3 className="text-2xl font-semibold mb-4">{offer.title}</h3>
+              <p className="text-gray-600 mb-6">{offer.description}</p>
+              <button className="px-6 py-3 bg-[#232425] hover:bg-[#113a60] text-white text-lg font-semibold rounded-md">
+                {offer.buttonText}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SpecialOffers;
