dd306e5db47973a365183b021f848b32fab3dc5c
MxAziz <hereabdulaziz@gmail.com>
2024-12-22 12:15:38 +0600

    successfully added firebase setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+  import { ToastContainer, toast } from "react-toastify";
+  import "react-toastify/dist/ReactToastify.css";
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from './Layouts/MainLayout.jsx';
 import ErrorPage from './Components/ErrorPage.jsx';
+import AuthProvider from './Provider/AuthProvider.jsx';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +18,20 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={true}
+        draggable={true}
+        pauseOnHover={true}
+        theme="colored"
+      />
+    </AuthProvider>
   </StrictMode>
 );
