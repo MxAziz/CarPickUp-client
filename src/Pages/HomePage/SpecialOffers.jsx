2b1c71d16da3a582ac15040f873d5583e0fbcf39
MxAziz <hereabdulaziz@gmail.com>
2025-02-12 08:54:31 +0600

    dark mode added on the special offers page

--- src/Pages/HomePage/SpecialOffers.jsx
+++ src/Pages/HomePage/SpecialOffers.jsx
@@ -18,9 +18,9 @@ const SpecialOffers = () => {
   ];
 
   return (
-    <section className="py-16 bg-gray-50">
+    <section className="py-16 bg-gray-50 dark:dark:bg-[#323538]">
       <div className="container mx-auto px-6">
-        <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
+        <h2 className="text-3xl md:text-4xl font-bold text-center mb-12 dark:text-white">
           Special Offers
         </h2>
         {/* 2 offer card */}
@@ -28,10 +28,10 @@ const SpecialOffers = () => {
           {offers.map((offer, index) => (
             <div
               key={index}
-              className="p-6 bg-white rounded-lg shadow-lg transform   hover:scale-105 transition-transform duration-300"
+              className="p-6 bg-white dark:bg-[#232425] rounded-lg shadow-lg transform   hover:scale-105 transition-transform duration-300"
             >
-              <h3 className="text-2xl font-semibold mb-4">{offer.title}</h3>
-              <p className="text-gray-600 mb-6">{offer.description}</p>
+              <h3 className="text-2xl font-semibold mb-4 dark:text-gray-100">{offer.title}</h3>
+              <p className="text-gray-600 dark:text-gray-300 mb-6">{offer.description}</p>
               <NavLink to={"/availableCars"}>
                 <button className="px-6 py-3 bg-[#136b7a] hover:bg-[#232525] text-white text-lg font-semibold rounded-md">
                   {offer.buttonText}
