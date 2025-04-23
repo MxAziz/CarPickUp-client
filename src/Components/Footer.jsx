930dd8c6d8602c80e42406b9d111ca748b544589
MxAziz <hereabdulaziz@gmail.com>
2025-04-23 21:36:48 +0600

    update footer section and added quick contact list

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaLocationDot } from 'react-icons/fa6';
+import { FaLocationDot, FaWhatsapp } from 'react-icons/fa6';
 import { FiPhoneCall } from 'react-icons/fi';
 import { MdEmail } from 'react-icons/md';
 
@@ -8,32 +8,32 @@ const Footer = () => {
       <div className="">
         <footer className="bg-[#232425] text-white py-20">
           {/* top contact section */}
-          <div className="flex  flex-col lg:flex-row justify-between items-center mb-8 border border-black w-11/12 mx-auto px-14 py-4 rounded-lg">
-            <div className="flex gap-4 items-center justify-center mb-4">
+          <div className="flex flex-col lg:flex-row lg:justify-between items-start mb-8 border border-black w-11/12 mx-auto px-4 py-4 rounded-lg gap-6">
+            <div className="flex gap-4 items-start">
               <div className="p-6 rounded-full bg-[#136b7a] text-white">
-                <FiPhoneCall className=" size-6" />
+                <FiPhoneCall className="size-6" />
               </div>
-              <div className="">
+              <div>
                 <h3>Call us</h3>
                 <p>+880 123456789</p>
               </div>
             </div>
 
-            <div className="flex gap-4 items-center justify-center mb-4">
+            <div className="flex gap-4 items-start">
               <div className="p-6 rounded-full bg-[#136b7a] text-white">
-                <MdEmail className=" size-6" />
+                <MdEmail className="size-6" />
               </div>
-              <div className="">
+              <div>
                 <h3>Write to us</h3>
                 <p>info@carpickup.com</p>
               </div>
             </div>
 
-            <div className="flex gap-4 items-center justify-center mb-4">
+            <div className="flex gap-4 items-start">
               <div className="p-6 rounded-full bg-[#136b7a] text-white">
-                <FaLocationDot className=" size-6" />
+                <FaLocationDot className="size-6" />
               </div>
-              <div className="">
+              <div>
                 <h3>Address</h3>
                 <p>Rajshahi, City Center, Office 123</p>
               </div>
@@ -49,9 +49,15 @@ const Footer = () => {
                 A simple and user-friendly car rental system to book, manage,
                 and track vehicles online.
               </small>
-              <small>©MxAziz{new Date().getFullYear()} | All Rights Reserved</small>
+              <small>
+                ©MxAziz{new Date().getFullYear()} | All Rights Reserved
+              </small>
               {/* social buttons */}
-              <div className=""></div>
+              <div className="">
+                <span className=' border-2 p-6 rounded-full '>
+                <FaWhatsapp className=" size-6"></FaWhatsapp>
+                </span>
+              </div>
             </div>
             {/* middle */}
             <div className=""></div>
